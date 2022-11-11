333849503f1846825e0dbfb5030a324cb7bf3f7d
Kenvix Zure <kenvixzure@live.com>
2022-11-11 17:55:11 +0800

    Add OpenGL Resources

--- PreBuild.bat
+++ PreBuild.bat
@@ -3,3 +3,6 @@ cd /d %~dp0
 mkdir cmake-build-debug\bin\x64
 for /f %%f in ('dir /b bin\x64\') do mklink /H cmake-build-debug\bin\x64\%%f bin\x64\%%f
 for /f %%f in ('dir /b bin\') do mklink /H cmake-build-debug\bin\%%f bin\%%f
+
+for /f %%f in ('dir /b resources\') do if exist resources\%%f\* mklink /D cmake-build-debug\bin\x64\%%f ..\..\..\resources\%%f
+for /f %%f in ('dir /b resources\') do if exist resources\%%f mklink cmake-build-debug\bin\x64\%%f ..\..\..\resources\%%f
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <cstdio>  /* defines FILENAME_MAX */
+//#define WINDOWS  /* uncomment this line to use it for windows.*/
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(){
+    char buff[FILENAME_MAX];
+    GetCurrentDir( buff, FILENAME_MAX );
+    printf("Current working dir: %s\n", buff);
+    return 1;
 }
