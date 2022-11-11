2d65d068e91bea2bab37a156aa6481d32ee478a4
Kenvix Zure <kenvixzure@live.com>
2022-11-11 18:03:53 +0800

    Prebuild enhanced

--- PreBuild.bat
+++ PreBuild.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d %~dp0
-mkdir cmake-build-debug\bin\x64
-for /f %%f in ('dir /b bin\x64\') do mklink /H cmake-build-debug\bin\x64\%%f bin\x64\%%f
-for /f %%f in ('dir /b bin\') do mklink /H cmake-build-debug\bin\%%f bin\%%f
+if not exist cmake-build-debug\bin\x64\. mkdir cmake-build-debug\bin\x64
+for /f %%f in ('dir /b bin\x64\') do if not exist cmake-build-debug\bin\x64\%%f\. mklink /H cmake-build-debug\bin\x64\%%f bin\x64\%%f
+for /f %%f in ('dir /b bin\') do if not exist cmake-build-debug\bin\%%f\. mklink /H cmake-build-debug\bin\%%f bin\%%f
 
-for /f %%f in ('dir /b resources\') do if exist resources\%%f\* mklink /D cmake-build-debug\bin\x64\%%f ..\..\..\resources\%%f
-for /f %%f in ('dir /b resources\') do if exist resources\%%f mklink cmake-build-debug\bin\x64\%%f ..\..\..\resources\%%f
+for /f %%f in ('dir /b resources\') do if exist resources\%%f\* if not exist cmake-build-debug\bin\x64\%%f\. mklink /D cmake-build-debug\bin\x64\%%f ..\..\..\resources\%%f
+for /f %%f in ('dir /b resources\') do if exist resources\%%f if not exist cmake-build-debug\bin\x64\%%f mklink /H cmake-build-debug\bin\x64\%%f resources\%%f
