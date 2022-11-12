6fb7f47895ae25c65fad7868ea6c250f47fc6ce1
Kenvix Zure <kenvixzure@live.com>
2022-11-12 21:09:08 +0800

    Dynamic PreBuild.bat

--- PreBuild.bat
+++ PreBuild.bat
@@ -1,8 +1,14 @@
 @echo off
 cd /d %~dp0
-if not exist cmake-build-debug\bin\x64\. mkdir cmake-build-debug\bin\x64
-for /f %%f in ('dir /b bin\x64\') do if not exist cmake-build-debug\bin\x64\%%f\. mklink /H cmake-build-debug\bin\x64\%%f bin\x64\%%f
-for /f %%f in ('dir /b bin\') do if not exist cmake-build-debug\bin\%%f\. mklink /H cmake-build-debug\bin\%%f bin\%%f
+set CMakeOutputDir=%1
+IF "%CMakeOutputDir%"=="" (
+    set CMakeOutputDir=cmake-build-debug
+)
+echo CMAKE output dir: %CMakeOutputDir%
 
-for /f %%f in ('dir /b resources\') do if exist resources\%%f\* if not exist cmake-build-debug\bin\x64\%%f\. mklink /D cmake-build-debug\bin\x64\%%f ..\..\..\resources\%%f
-for /f %%f in ('dir /b resources\') do if exist resources\%%f if not exist cmake-build-debug\bin\x64\%%f mklink /H cmake-build-debug\bin\x64\%%f resources\%%f
+if not exist %CMakeOutputDir%\bin\x64\. mkdir %CMakeOutputDir%\bin\x64
+for /f %%f in ('dir /b bin\x64\') do if not exist %CMakeOutputDir%\bin\x64\%%f\. mklink /H %CMakeOutputDir%\bin\x64\%%f bin\x64\%%f
+for /f %%f in ('dir /b bin\') do if not exist %CMakeOutputDir%\bin\%%f\. mklink /H %CMakeOutputDir%\bin\%%f bin\%%f
+
+for /f %%f in ('dir /b resources\') do if exist resources\%%f\* if not exist %CMakeOutputDir%\bin\x64\%%f\. mklink /D %CMakeOutputDir%\bin\x64\%%f ..\..\..\resources\%%f
+for /f %%f in ('dir /b resources\') do if exist resources\%%f if not exist %CMakeOutputDir%\bin\x64\%%f mklink /H %CMakeOutputDir%\bin\x64\%%f resources\%%f
