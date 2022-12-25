b55c1b7d1e02d2bb68ca88a28699468fa8064013
Kenvix Zure <kenvixzure@live.com>
2022-12-25 19:57:59 +0800

    Split package settings (2)

--- src/main/3dmodeling/CGALTest.cpp
+++ src/main/3dmodeling/CGALTest.cpp
@@ -1,10 +1,17 @@
-//
-// Created by Kenvix on 2022/12/25.
-//
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/poisson_surface_reconstruction.h>
+#include <CGAL/IO/read_points.h>
+#include <vector>
+#include <fstream>
 
-#include <iostream>
+// Types
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_3 Point;
+typedef Kernel::Vector_3 Vector;
+typedef std::pair<Point, Vector> Pwn;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
